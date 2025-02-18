cf0f2c5efc2174fa73c0c2621a8232fbd9494f39
Shivraj Roy <shivrajroy78@gmail.com>
2025-02-18 23:26:41 +0530

    Add ProjectSidebar component; update dependencies...

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import ProjectSidebar from "./components/ProjectSidebar";
+
 function App() {
    return (
       <>
-         <h1 className="flex-auto justify-center mx-8 text-amber-700 font-bold">
-            ProjectHub
-         </h1>
-         <p>chal ja bhao...</p>
+         <ProjectSidebar />
       </>
    );
 }
---
+++ src/components/ProjectSidebar.jsx
@@ -0,0 +1,22 @@
+import { CiCirclePlus } from "react-icons/ci";
+import LOGO from "../assets/PH Logo.png";
+
+export default function ProjectSidebar() {
+   return (
+      <aside className="w-80 h-screen bg-white border-r border-gray-200">
+         <div className="flex items-center justify-center space-x-2 p-4 border-b border-gray-200">
+            <img
+               className="w-18 h-18 object-cover rounded-full"
+               src={LOGO}
+               alt="projectHub-logo"
+            />
+            <h1 className="font-display font-bold text-3xl">ProjectHub</h1>
+         </div>
+         <div className="px-4 py-2">
+            <button className="flex items-center justify-center gap-4 w-full p-4 text-lg font-semibold cursor-pointer rounded-4xl text-white bg-gray-600 hover:bg-gray-800">
+               <CiCirclePlus className="text-4xl" /> Add Project
+            </button>
+         </div>
+      </aside>
+   );
+}
